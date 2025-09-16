4a3daee7c7abf29b65dcbb99abefaa8c9971aa1c
Marc Towler <marc@marctowler.co.uk>
2025-09-16 23:19:55 +0100

    hgmm

--- Images/Runner/entrypoint.sh
+++ Images/Runner/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Ensure work dir exists and has correct ownership
 mkdir -p /home/runner/actions-runner/_work
-chown -R runner:runner /home/runner/actions-runner/_work
+mkdir -p /home/runner/actions-runner/_tool
+chown -R runner:docker /home/runner/actions-runner
 
-# Drop privileges to runner
-exec su runner -c "/runner-entry.sh"
+# Drop to non-root user (runner)
+exec su - runner -c "/runner-entry.sh"
