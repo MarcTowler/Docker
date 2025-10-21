a7457e73b8d6a6d7fed9a4e503aee8eff56b6b26
Marc Towler <marc@marctowler.co.uk>
2025-10-21 22:16:53 +0100

    Fixed issues

--- Scripts/setup-bind-and-traefik.sh
+++ Scripts/setup-bind-and-traefik.sh
@@ -3,7 +3,7 @@ docker network create -d overlay traefik-net
 echo "=== Overlay network 'traefik-net' created ==="
 
 # Deploy BIND9 Service
-docker stack deploy -c ../bind9/bind9-stack.yml bind9
+docker stack deploy -c ../bind9/bind9-stack.yml
 echo "=== BIND9 service deployed ==="
 
 # Generate TLS Certificates for Traefik
@@ -12,33 +12,12 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/traefik/certs/trae
 echo "=== TLS certificates for Traefik generated ==="
 
 # Deploy Traefik Service
-docker stack deploy -c ../Traefik/traefik-stack.yml traefik
+docker stack deploy -c ../Traefik/traefik-stack.yml
 echo "=== Traefik service deployed ==="
 
 echo "=== Setup complete: BIND9 and Traefik are running on 'traefik-net' overlay network ==="
 
-# Build Test App
-cat > whoami.yml << EOF
-version: "3.9"
-
-services:
-  whoami:
-    image: traefik/whoami
-    networks:
-      - traefik-net
-    deploy:
-      labels:
-        - "traefik.enable=true"
-        - "traefik.http.routers.whoami.rule=Host(`whoami.itslit`)"
-        - "traefik.http.routers.whoami.entrypoints=websecure"
-        - "traefik.http.services.whoami.loadbalancer.server.port=80"
-
-networks:
-  traefik-net:
-    external: true
-EOF
-
 # Deploy Test App
-docker stack deploy -c whoami.yml whoami
+docker stack deploy -c ../Traefik/whoami.yml
 echo "=== Test app 'whoami' deployed ==="
 echo "=== You can access the test app at https://whoami.itslit ==="
