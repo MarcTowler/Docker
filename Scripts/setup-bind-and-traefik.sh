b8d32ef122df4753d49f0f9f0a5d9e4525d14f7b
Marc Towler <marc@marctowler.co.uk>
2025-10-21 22:18:31 +0100

    re-added stack arg

--- Scripts/setup-bind-and-traefik.sh
+++ Scripts/setup-bind-and-traefik.sh
@@ -3,7 +3,7 @@ docker network create -d overlay traefik-net
 echo "=== Overlay network 'traefik-net' created ==="
 
 # Deploy BIND9 Service
-docker stack deploy -c ../bind9/bind9-stack.yml
+docker stack deploy -c ../bind9/bind9-stack.yml bind9
 echo "=== BIND9 service deployed ==="
 
 # Generate TLS Certificates for Traefik
@@ -12,12 +12,12 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/traefik/certs/trae
 echo "=== TLS certificates for Traefik generated ==="
 
 # Deploy Traefik Service
-docker stack deploy -c ../Traefik/traefik-stack.yml
+docker stack deploy -c ../Traefik/traefik-stack.yml traefik
 echo "=== Traefik service deployed ==="
 
 echo "=== Setup complete: BIND9 and Traefik are running on 'traefik-net' overlay network ==="
 
 # Deploy Test App
-docker stack deploy -c ../Traefik/whoami.yml
+docker stack deploy -c ../Traefik/whoami.yml whoami
 echo "=== Test app 'whoami' deployed ==="
 echo "=== You can access the test app at https://whoami.itslit ==="
